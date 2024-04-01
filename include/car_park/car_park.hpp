d05e221b0ff4ab4895b00ff2103f3a17252abf32
hp <hp@HP-PC>
2024-04-01 09:19:54 +0300

    fix: compile single library

--- include/car_park/car_park.hpp
+++ include/car_park/car_park.hpp
@@ -1,9 +1,8 @@
 
 #pragma once
 
-namespace car_park {
-
-
-int add_one(int x);
-
-}
+#include "car_park/user.hpp"
+#include "car_park/driver.hpp"
+#include "car_park/order.hpp"
+#include "car_park/car.hpp"
+#include "car_park/moneyPerPeriod.hpp"
