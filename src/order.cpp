563c6ec6c35f89c26356e9149020e9e076a2a38d
hp <hp@HP-PC>
2024-04-03 11:36:00 +0300

    Update function count_order_per_period

--- src/order.cpp
+++ src/order.cpp
@@ -27,6 +27,41 @@ namespace car_park {
         return order->getCargoWeight() <= car->getCapacity();
     }
 
+    void OrdersDAO::count_order_per_period(car_park::User &user, car_park::Driver &driver,
+                                           std::vector<Order> &orders_of_driver, long long start_date,
+                                           long long end_date) {
+        if (!User::check_access(user))
+            return;
+        sqlite3 *db;
+        int rc = sqlite3_open("../../autopark.db", &db);
+        if (rc != SQLITE_OK)
+            return;
+        std::string sql = "SELECT id,datetime,driver_id,car_number,length,cargo_weight,cost FROM orders WHERE driver_id = ? AND datetime >= ? AND datetime <= ?";
+        sqlite3_stmt *stmt;
+        int count = 0;
+
+        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) == SQLITE_OK) {
+            sqlite3_bind_int64(stmt, 1, driver.getId());
+            sqlite3_bind_int64(stmt, 2, start_date);
+            sqlite3_bind_int64(stmt, 3, end_date);
+
+            while (sqlite3_step(stmt) == SQLITE_ROW) {
+                int id = sqlite3_column_int(stmt, 0);
+                int datetime = sqlite3_column_int(stmt, 1);
+                int driver_id = sqlite3_column_int(stmt, 2);
+                std::string car_number = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
+                double length = sqlite3_column_double(stmt, 4);
+                double cargo_weight = sqlite3_column_double(stmt, 5);
+                double cost = sqlite3_column_double(stmt, 6);
+
+                orders_of_driver.emplace_back(Order(std::to_string(id) + "," + std::to_string(datetime) + "," + std::to_string(driver_id) + ","  +
+                                                    car_number + "," + std::to_string(length) + "," + std::to_string(cargo_weight) + "," + std::to_string(cost)));
+            }
+            sqlite3_finalize(stmt);
+        }
+        sqlite3_close(db);
+    }
+
     void OrdersDAO::find_all_by_driver(User& user, Driver& driver, std::vector<Order>& orders_of_driver){
         if (!User::check_access(user))
             return;
