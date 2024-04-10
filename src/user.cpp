ee8e9867e2a4c178a165523d59a4c8e18cf32e5f
IronGun <strannik.kraya575721@gmail.com>
2024-04-10 23:11:25 +0300

    fucking fuck

--- src/user.cpp
+++ src/user.cpp
@@ -1,6 +1,8 @@
 #include "car_park/user.hpp"
 #include "sql/sqlite3.h"
 #include <sstream>
+#include <iostream>
+#include <filesystem>
 
 
 
@@ -26,6 +28,7 @@ namespace car_park {
     }
 
     User *UsersDAO::find(std::string login, std::string password) {
+        std::cout << "current_path " << std::filesystem::current_path() << std::endl;
         sqlite3 *db;
         int rc = sqlite3_open("../../autopark.db", &db);
         if (rc != SQLITE_OK)
@@ -51,15 +54,20 @@ namespace car_park {
 
     bool UsersDAO::insert(User &user, std::string password) {
         sqlite3 *db;
+        std::cout << "current_path " << std::filesystem::current_path() << std::endl;
         int rc = sqlite3_open("../../autopark.db", &db);
         if (rc != SQLITE_OK)
             return false;
         std::string sql = "INSERT INTO users (login, password, role) VALUES (?, ?, ?)";
         sqlite3_stmt* stmt;
         if(sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) == SQLITE_OK) {
-            sqlite3_bind_text(stmt, 1, user.get_login().c_str(), -1, SQLITE_STATIC);
-            sqlite3_bind_text(stmt, 2, password.c_str(), -1, SQLITE_STATIC);
-            sqlite3_bind_text(stmt, 3, user.get_type().c_str(), -1, SQLITE_STATIC);
+            sqlite3_bind_text(stmt, 1, user.get_login().c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_text(stmt, 2, password.c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_text(stmt, 3, user.get_type().c_str(), -1, SQLITE_TRANSIENT);
+            const char *a = sqlite3_sql(stmt);
+//            std::string strasd = sqlite3_sql;
+            char *b = sqlite3_expanded_sql(stmt);
+//            const char *c = sqlite3_normalized_sql(stmt);
             rc = sqlite3_step(stmt);
             if (rc != SQLITE_DONE) {
                 sqlite3_finalize(stmt);
